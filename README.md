47d74e87b912f6bf0643132445f682eadeab5cd8
sebalander <seba.arroyo7@gmail.com>
2015-03-08 12:54:57 -0300

    program working correctly. compares models 0 and 1

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # tegstatistics
 I'm probably not the first one to wonder wether some people are prone
 to get better dice rolls than others. TEG is a game highly dependent
-on dice rolling performance. I recently played TEG with other three
+on dice rolling performance. I recently played TEG with three other
 people and took the time to record each player's scores.
 
 I intend to analyse with elementary statistic tools the performance of
 each player. I want to find quantitatively if some people really are
 better dice throwers. 
 
-This is just for fun, I don't mean to go too deep in the formalism.
+This is just for fun, I don't mean to go too deep in the formalism. 
+Unless, of course, someone else fnds this interesting. Posible 
+applications if expanded are ...
--- tegstatistics.py
+++ tegstatistics.py
@@ -16,20 +16,26 @@ Lo=np.array([2, 3, 4, 3, 5, 4, 4, 2, 2, 2, 3, 4, 4, 1, 5, 5, 4, 3, 2, 1, 2, 3, 6
 Lu=np.array([2, 1, 2, 1, 6, 6, 1, 4, 3, 2, 6, 2, 2, 2, 2, 4, 6, 5, 4, 6, 4, 6, 6, 4, 6, 1, 6, 4, 6, 4, 5, 6, 6, 5, 3, 3, 6, 5, 5, 6, 1, 4, 6, 3, 2, 4, 4, 1, 5, 5, 1, 6, 3, 5, 4, 2, 6, 4, 1, 6, 6, 2, 2, 5, 4])
 Go=np.array([1, 5, 4, 1, 6, 2, 6, 1, 4, 1, 4, 5, 6, 3, 3, 4, 4, 3, 5, 1, 4, 6, 4, 4, 6, 2, 6, 4, 6, 1, 5, 1, 5, 4, 1, 1, 4, 3, 3, 4, 3, 2, 6, 6, 6, 5, 5, 3, 3, 3, 4, 2, 4, 1])
 Se=np.array([4, 1, 1, 6, 6, 3, 2, 1, 3, 4, 5, 2, 1, 4, 3, 2, 5, 4, 6, 6, 6, 1, 5, 1, 2, 5, 3, 1, 6, 1, 5, 5, 2, 6, 2, 6, 5, 5, 5, 5, 3, 6, 6, 4, 2, 6, 6, 2, 2, 3])
-# list of list of data (not arra because different length)
+# list of data vectors (not array because of different length)
 Dat=[[Lo,Lu],[Go,Se]]
 # players names
 Jugadores=[['Lore','Lu'],['Gon','Seba']]
-# compute number of throws of each player
-tiradas=[[np.size(Lo),np.size(Lu)],[np.size(Go),np.size(Se)]]
-promedio=[[np.average(Lo),np.average(Lu)],[np.average(Go),np.average(Se)]]
 
 
 
-def savehistfigure(Dat,Jugadores,tiradas,promedio):
+
+def savehistfigure(Dat,Jugadores):
 	'''
 	Function that plots the histogram for each player.
 	'''
+	
+	# compute number of throws of each player
+	tiradas=[[np.size(Lo),np.size(Lu)],[np.size(Go),np.size(Se)]]
+	# average score
+	promedio=[[np.average(Lo),np.average(Lu)],
+		[np.average(Go),np.average(Se)]]
+	
+	
 	
 	xlabels=['', 1, 2, 3, 4, 5, 6, '']# x axis labels
 	
@@ -59,9 +65,10 @@ def savehistfigure(Dat,Jugadores,tiradas,promedio):
 				rwidth=0.9)
 	# once constructed all four histograms, save image
 	plt.savefig('histogramas.png')
+	plt.show()
 
 
-def comparemodels(Dat,Jugadores):
+def comparemodels01(Dat,Jugadores):
 	'''
 	I propose two models for the probability of each dice face.
 	Model 0 consists of equal constant probability for each face
@@ -74,33 +81,51 @@ def comparemodels(Dat,Jugadores):
 	linear function.
 	'''
 	
-	# get frequency of appearance of each score
 	freq=np.ndarray([2,2,6]) # variable to save feqs
-	for i in range(2):
-		for j in range(2):
-			freq[i,j]=np.histogram(Dat[i][j],6,(0.5,6.5))[0]
-	
-	atics=20 # Number of intervals to plot a
-	a_range=np.linspace(0,7.0/12,atics) # range of posible slopes
+	atics=200 # Number of intervals to plot slope
+	a_range=np.linspace(-1.0/15,1.0/15,atics) # range of posible slopes
 	prob_a=np.ndarray([2,2,atics]) # to save prob dist of a
 	
-	# calculate prob dist of slope for each player
+	p6=1.0/6 # probability if uniform
+	
+	# calculate prob dist of data given slope for each player 
+	#do with respect to prob of data given model 0 to take results 
+	#of each player to same level. They differ in number of throws.
 	for i in range(2):
 		for j in range(2):
-			# evaluate probability at each a
-			# function comes out from ...
-			prob_a[i,j]=[
-				np.sum([ ((l-3.5)*a_range[k]+1.0/6)**freq[i][j]
-				 for l in range(1,7)])
-				for k in range(atics)]
+			# get frequency of appearance of each score
+			freq[i,j]=np.histogram(Dat[i][j],6,(0.5,6.5))[0]
+			prob_model0=1.0/(6**np.sum(freq[i,j]))
+			# evaluate probability of data given slope
+			# function comes from ...
+			prob_a[i,j]=[ 
+				np.prod(    [
+					 ((l-3.5)*a_range[k]+p6) 
+					 for l in range(1,7)]
+					 	**freq[i][j]   )
+				for k in range(atics)] /  prob_model0#[i,j]
+				#don't know how to do the normalization more 
+				#efficiently...
+	
+	
+	
+#	prob_model0=1.0/(6**np.sum(freq,2)) # (1/6)**(Number of rolls)
+	#probability of data given model 1 (integrated over slope values) 
+	#with respect to probability of data given Model 0.
+	d_a=1.0/15/atics # columns width, useful for integration
+	mod1_mod0=np.sum(prob_a,2)*d_a#/  prob_model0
+	
+	print mod1_mod0# , prob_model0
 	
 	# now plot
 	fig, axis = plt.subplots(2,2)# create four plots
+	probmax = np.max(np.max(np.max(prob_a))) # limit of y axis
 	for i in range(2):
 		for j in range(2):
 			# leyend to be displayed on graph
 #			axis[i,j].set_yticklabels([])
-#			axis[i,j].set_ylim([0,0.3])
+#			axis[i,j].set_ylim(0,probmax)
+#			axis[i,j].set_yscale('log')
 #			axis[i,j].set_yticks([])
 #			axis[i,j].set_xticklabels(xlabels)
 #			axis[i,j].text(0.3, 0.23, txt)
@@ -108,7 +133,11 @@ def comparemodels(Dat,Jugadores):
 #			axis[i,j].plot([0.5,6.5],[prob, prob])
 			axis[i,j].plot(a_range,prob_a[i][j])
 	# once constructed all four histograms, save image
-	plt.savefig('aProbdist.png')
+#	plt.savefig('aProbdist.png')
+	plt.show()
+	
+	# return the relative probability of the models for each pplayer
+	return mod1_mod0
 
 
 
@@ -116,14 +145,18 @@ def comparemodels(Dat,Jugadores):
 
 
 
-comparemodels(Dat,Jugadores)
+comparemodels01(Dat,Jugadores)
 
 
 
+# auxiliar space to check formula
+#Iter_l= ((l-3.5)*a_range[k]+p6)
 
+#Iter_k=np.prod(    [ Iter_l for l in range(1,7)]**freq[i][j]   )
 
+#prob_a[i,j]=[ Iter_k for k in range(atics)]
 
-
+#[ np.prod(    [ ((l-3.5)*a_range[k]+p6) for l in range(1,7)]**freq[i][j]   ) for k in range(atics)]
 
 
 
